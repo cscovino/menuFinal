048760f6a3c9aa754c1507c237ad2b3aaf3c1693
cscovino <kikescovino@gmail.com>
2017-09-23 18:40:04 -0400

    test

--- menuFinal/www/js/index.js
+++ menuFinal/www/js/index.js
@@ -977,6 +977,14 @@ if ('addEventListener' in document) {
     }, false);
 }
 
+$(document).on('show.bs.modal', '.modal', function () {
+    var zIndex = 1040 + (10 * $('.modal:visible').length);
+    $(this).css('z-index', zIndex);
+    setTimeout(function() {
+        $('.modal-backdrop').not('.modal-stack').css('z-index', zIndex - 1).addClass('modal-stack');
+    }, 0);
+});
+
 firebase.database().ref().on('value', function(snap){
     if (snap.val() !== null) {
         app.setSnap(snap.val());
